88b786de51a9ba9a6f56c3531bc18c25575abe13
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-09-08 00:58:20 -0700

    Use Prettify from ts-essentials

--- shared/shared.ts
+++ shared/shared.ts
@@ -1,21 +1,26 @@
 import * as db from "server/database-types";
 import type { Repository } from "server/repository";
+import { Prettify } from "ts-essentials";
 
 export interface ExampleMessage {
   value: string;
 }
 
-export type Hangout = db.Hangout & {
-  friends: db.Friend[];
-};
+export type Hangout = Prettify<
+  db.Hangout & {
+    friends: db.Friend[];
+  }
+>;
 
 export type Friend = db.Friend;
 
 export type NewFriend = Pick<db.NewFriend, "name">;
 
-export type NewHangout = Omit<db.NewHangout, "owner_id"> & {
-  friends: number[];
-};
+export type NewHangout = Prettify<
+  Omit<db.NewHangout, "owner_id"> & {
+    friends: number[];
+  }
+>;
 
 export type MyFriendsResponse = ReturnType<Repository["getMyFriends"]>;
 export type MyHangoutsResponse = Hangout[];
