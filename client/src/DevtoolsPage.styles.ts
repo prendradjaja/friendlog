4d19f5c3021665b23091d1bffca1565d84ce1666
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-09-08 10:45:05 -0700

    Move existing content to DevtoolsPage, prep styles for Radix

---
+++ client/src/DevtoolsPage.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+export default styled.div`
+  padding: 10px;
+  font-family: sans-serif;
+
+  * {
+    font-size: 20px;
+  }
+`;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HomePage } from "./HomePage";
+import { DevtoolsPage } from "./DevtoolsPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/devtools",
+    element: <DevtoolsPage />,
   },
 ]);
 
