5147e925bd66755fea17d258d80fd7347ae86b9e
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-09-12 10:52:57 -0700

    Add session table

--- server/raw-database-types.ts
+++ server/raw-database-types.ts
@@ -10,6 +10,18 @@ export type Generated<T> =
     ? ColumnType<S, I | undefined, U>
     : ColumnType<T, T | undefined, T>;
 
+export type Json = JsonValue;
+
+export type JsonArray = JsonValue[];
+
+export type JsonObject = {
+  [x: string]: JsonValue | undefined;
+};
+
+export type JsonPrimitive = boolean | number | string | null;
+
+export type JsonValue = JsonArray | JsonObject | JsonPrimitive;
+
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
 export interface Account {
@@ -43,10 +55,17 @@ export interface Hangout {
   title: string;
 }
 
+export interface Session {
+  expire: Timestamp;
+  sess: Json;
+  sid: string;
+}
+
 export interface DB {
   account: Account;
   federated_credential: FederatedCredential;
   friend: Friend;
   friend_hangout: FriendHangout;
   hangout: Hangout;
+  session: Session;
 }
