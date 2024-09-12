8828ad0095784c3d01fa82b7c62b7bb1f192699c
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-09-12 16:31:28 -0700

    Add hangout dropdown & delete hangout skeleton

--- client/src/HangoutsPage.tsx
+++ client/src/HangoutsPage.tsx
@@ -1,6 +1,16 @@
 import * as api from "./api";
 import { LoginStatus, Hangout } from "shared";
-import { Card, Link, Text } from "@radix-ui/themes";
+import { DotsVerticalIcon, HamburgerMenuIcon } from "@radix-ui/react-icons";
+import {
+  Card,
+  Link,
+  Text,
+  AlertDialog,
+  Dialog,
+  Flex,
+  TextField,
+  DropdownMenu,
+} from "@radix-ui/themes";
 import {
   LoaderFunctionArgs,
   Link as RouterLink,
@@ -34,19 +44,35 @@ export function HangoutsPage() {
           <RouterLink to="/">Friendlog</RouterLink>
         </Link>
       </div>
+
       {hangouts.map((hangout) => (
         <Card key={hangout.id}>
-          <div>
-            {hangout.friends.map((friend, i, friends) => (
-              <React.Fragment key={friend.id}>
-                <Link asChild weight="bold">
-                  <RouterLink to={"/friends/" + friend.id}>
-                    {friend.name}
-                  </RouterLink>
-                </Link>
-                {i < friends.length - 1 && <Text>{", "}</Text>}
-              </React.Fragment>
-            ))}
+          <div style={{ display: "flex", width: "100%" }}>
+            <div style={{ flexGrow: "1" }}>
+              {hangout.friends.map((friend, i, friends) => (
+                <React.Fragment key={friend.id}>
+                  <Link asChild weight="bold">
+                    <RouterLink to={"/friends/" + friend.id}>
+                      {friend.name}
+                    </RouterLink>
+                  </Link>
+                  {i < friends.length - 1 && <Text>{", "}</Text>}
+                </React.Fragment>
+              ))}
+            </div>
+            <div>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <IconButton variant="ghost">
+                    <HamburgerMenuIcon />
+                  </IconButton>
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Item disabled>Edit</DropdownMenu.Item>
+                  <DropdownMenu.Item>Delete</DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            </div>
           </div>
           <Text as="div" size="2" color="gray">
             {formatRelativeToToday(hangout.hangout_date_string)}
