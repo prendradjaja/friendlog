391e530d947a17a1b4666f226040d5d1d8dbf739
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-09-12 10:41:58 -0700

    Secrets management

--- server/config.ts
+++ server/config.ts
@@ -1,3 +1,9 @@
+import * as fs from "fs";
+
+type GetSecretFunction = (secretKey: string) => string;
+
+const localSecretsPath = "./local-secrets.json";
+
 export interface Config {
   port: string;
   allowAnyOrigin: boolean;
@@ -7,34 +13,43 @@ export interface Config {
   databaseUrl: string;
   useSSLForDatabaseConnection: boolean;
   fakeNetworkDelay: number | undefined; // In milliseconds
+  googleClientID: string;
+  googleClientSecret: string;
 }
 
-export const devConfig: Config = {
-  port: "2201",
-  allowAnyOrigin: true,
-  allowAnyHeaders: true,
-  allowAnyMethods: true,
-  staticFilesPath: undefined,
-  databaseUrl: "postgres://localhost:5432/friendlog",
-  useSSLForDatabaseConnection: false,
-  fakeNetworkDelay: 100,
-};
+const loadDevConfig = (getSecret: GetSecretFunction) =>
+  ({
+    port: "2201",
+    allowAnyOrigin: true,
+    allowAnyHeaders: true,
+    allowAnyMethods: true,
+    staticFilesPath: undefined,
+    databaseUrl: "postgres://localhost:5432/friendlog",
+    useSSLForDatabaseConnection: false,
+    fakeNetworkDelay: 100,
+    googleClientID: getSecret("GOOGLE_CLIENT_ID"),
+    googleClientSecret: getSecret("GOOGLE_CLIENT_SECRET"),
+  }) satisfies Config;
 
-const loadProductionConfig: () => Config = () => ({
-  port: getEnvironmentVariable("PORT"),
-  allowAnyOrigin: false,
-  allowAnyHeaders: false,
-  allowAnyMethods: false,
-  staticFilesPath: "../client/dist/",
-  databaseUrl: getEnvironmentVariable("DATABASE_URL"),
-  useSSLForDatabaseConnection: true,
-  fakeNetworkDelay: undefined,
-});
+const loadProductionConfig: () => Config = () =>
+  ({
+    port: getEnvironmentVariable("PORT"),
+    allowAnyOrigin: false,
+    allowAnyHeaders: false,
+    allowAnyMethods: false,
+    staticFilesPath: "../client/dist/",
+    databaseUrl: getEnvironmentVariable("DATABASE_URL"),
+    useSSLForDatabaseConnection: true,
+    fakeNetworkDelay: undefined,
+    googleClientID: getEnvironmentVariable("GOOGLE_CLIENT_ID"),
+    googleClientSecret: getEnvironmentVariable("GOOGLE_CLIENT_SECRET"),
+  }) satisfies Config;
 
 export function loadConfig(): Config {
   const isDev = process.env.DEV === "true";
   if (isDev) {
-    return devConfig;
+    const getSecret = getLocalSecrets();
+    return loadDevConfig(getSecret);
   } else {
     return loadProductionConfig();
   }
@@ -46,4 +61,21 @@ function getEnvironmentVariable(name: string): string {
     throw new Error("Missing environment variable: " + name);
   }
   return value;
+}
+
+function getLocalSecrets() {
+  if (!fs.existsSync(localSecretsPath)) {
+    throw new Error("Missing secrets file (required for local development)");
+  }
+  const secrets: Partial<Record<string, string>> = JSON.parse(
+    fs.readFileSync("./local-secrets.json", "utf8"),
+  );
+
+  return (secretKey: string): string => {
+    const value = secrets[secretKey];
+    if (value === undefined) {
+      throw new Error("Missing secret: " + secretKey);
+    }
+    return value;
+  };
 }
