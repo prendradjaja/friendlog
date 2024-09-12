c9d33704b53ebaa27a4de7d9078847d24af9f03a
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-09-12 16:46:48 -0700

    Clean up HangoutCard

--- client/src/HangoutCard.styles.ts
+++ client/src/HangoutCard.styles.ts
@@ -1,2 +1,10 @@
 import styled from "styled-components";
-export default styled.div``;
+export default styled.div`
+  .hangout-header {
+    display: flex;
+
+    :first-child {
+      flex-grow: 1;
+    }
+  }
+`;
--- client/src/HangoutCard.tsx
+++ client/src/HangoutCard.tsx
@@ -7,41 +7,45 @@ import { IconButton, Button } from "@radix-ui/themes";
 import StyleWrapper from "./HangoutCard.styles";
 import * as React from "react";
 import { formatRelativeToToday } from "./date-util";
+import { useState } from "react";
 
 interface Props {
   hangout: Hangout;
 }
 
 export function HangoutCard({ hangout }: Props) {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const friendNames = hangout.friends.map((friend, i, friends) => (
+    <React.Fragment key={friend.id}>
+      <Link asChild weight="bold">
+        <RouterLink to={"/friends/" + friend.id}>{friend.name}</RouterLink>
+      </Link>
+      {i < friends.length - 1 && <Text>{", "}</Text>}
+    </React.Fragment>
+  ));
+
+  const dropdown = (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <IconButton variant="ghost">
+          <HamburgerMenuIcon />
+        </IconButton>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        <DropdownMenu.Item disabled>Edit</DropdownMenu.Item>
+        <DropdownMenu.Item>Delete</DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  );
+
   return (
     <StyleWrapper>
       <Card key={hangout.id}>
-        <div style={{ display: "flex", width: "100%" }}>
-          <div style={{ flexGrow: "1" }}>
-            {hangout.friends.map((friend, i, friends) => (
-              <React.Fragment key={friend.id}>
-                <Link asChild weight="bold">
-                  <RouterLink to={"/friends/" + friend.id}>
-                    {friend.name}
-                  </RouterLink>
-                </Link>
-                {i < friends.length - 1 && <Text>{", "}</Text>}
-              </React.Fragment>
-            ))}
-          </div>
-          <div>
-            <DropdownMenu.Root>
-              <DropdownMenu.Trigger>
-                <IconButton variant="ghost">
-                  <HamburgerMenuIcon />
-                </IconButton>
-              </DropdownMenu.Trigger>
-              <DropdownMenu.Content>
-                <DropdownMenu.Item disabled>Edit</DropdownMenu.Item>
-                <DropdownMenu.Item>Delete</DropdownMenu.Item>
-              </DropdownMenu.Content>
-            </DropdownMenu.Root>
-          </div>
+        <div className="hangout-header">
+          <div>{friendNames}</div>
+          <div>{dropdown}</div>
         </div>
         <Text as="div" size="2" color="gray">
           {formatRelativeToToday(hangout.hangout_date_string)}
