645e8b3dd2d5d79d2171f5f37c5c67976c55c628
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-09-12 17:07:39 -0700

    AnonHomePage styling tweaks

---
+++ client/src/AnonHomePage.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+export default styled.div`
+  text-align: center;
+  margin-top: 200px;
+
+  h1 {
+    margin-bottom: var(--space-5);
+  }
+`;
--- client/src/AnonHomePage.tsx
+++ client/src/AnonHomePage.tsx
@@ -1,12 +1,16 @@
-import { Link } from "@radix-ui/themes";
+import { Link, Heading } from "@radix-ui/themes";
 import { redirect } from "react-router-dom";
 import { getLoginStatus } from "./login-status-store";
+import StyleWrapper from "./AnonHomePage.styles";
 
 export function AnonHomePage() {
   return (
-    <>
-      <Link href="/login/federated/google">Sign in with Google</Link>
-    </>
+    <StyleWrapper>
+      <div>
+        <Heading as="h1">Friendlog</Heading>
+        <Link href="/login/federated/google">Sign in with Google</Link>
+      </div>
+    </StyleWrapper>
   );
 }
 
