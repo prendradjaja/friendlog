8f0e58c133f9fff18a12ef3efbd3ff2016e72985
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-09-13 12:10:09 -0700

    Cleanup: Use a Map instead

--- server/routes.helpers.ts
+++ server/routes.helpers.ts
@@ -4,7 +4,8 @@ import { Hangout } from "shared";
 async function main() {
   const { Repository } = await import("./repository");
   const repo = new Repository();
-  const rows = await repo.getHangouts(3, 10);
+  const rows = await repo.getHangouts(3, 13);
+  console.table(rows);
   const results = unflattenHangouts(rows);
   console.log(JSON.stringify(results, null, 2));
 }
@@ -12,12 +13,12 @@ async function main() {
 export function unflattenHangouts(
   rows: Awaited<ReturnType<Repository["getHangouts"]>>,
 ): Hangout[] {
-  const results: Hangout[] = [];
+  const resultsMap: Map<number, Hangout> = new Map();
   for (const row of rows) {
     const { friend_id, friend_name, friend_owner_id, ...hangoutProperties } =
       row;
     let hangout: Hangout;
-    const match = results.find((result) => result.id === row.id);
+    const match = resultsMap.get(row.id);
     if (match !== undefined) {
       hangout = match;
     } else {
@@ -25,7 +26,7 @@ export function unflattenHangouts(
         ...hangoutProperties,
         friends: [],
       };
-      results.push(hangout);
+      resultsMap.set(row.id, hangout);
     }
     hangout.friends.push({
       id: friend_id,
@@ -33,6 +34,7 @@ export function unflattenHangouts(
       owner_id: friend_owner_id,
     });
   }
+  const results: Hangout[] = Array.from(resultsMap.values());
   return results;
 }
 
