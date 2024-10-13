ff766390258a643e73b9e6191567616a0f8bfaeb
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-10-13 12:11:03 -0700

    wip

---
+++ client/src/SelectFriends.tsx
@@ -0,0 +1,91 @@
+import { sortBy } from "./util";
+import CreatableSelect from "react-select/creatable";
+import { Friend } from "shared";
+import { InputProps, components, createFilter } from "react-select";
+import { useState } from "react";
+
+// todo Move Select stuff into a separate component?
+interface KnownSelectOption {
+  __isNew__: false;
+  value: number;
+  label: string;
+  friend: Friend;
+}
+
+interface NewSelectOption {
+  __isNew__: false;
+  value: string;
+  label: string;
+}
+
+type SelectOption = KnownSelectOption | NewSelectOption;
+
+const colors = {
+  reactSelectLightBlue: "hsl(216 100 92)",
+  radixIndigo5: "#D2DEFF",
+} as const;
+
+const Input = (props: InputProps<SelectOption>) => {
+  return <components.Input {...props} enterKeyHint="enter" />;
+};
+
+interface Props {
+  allFriends: Friend[];
+  initialValue: Value;
+  onChange: (value: Value) => void;
+}
+
+interface Value {
+  existingFriendIds: number[];
+  newFriendNames: string[];
+}
+
+export function SelectFriends({ allFriends, initialValue, onChange }: Props) {
+  const friendsAlphabetical = sortBy(allFriends, (x) => x.name);
+  const selectOptions = friendsAlphabetical.map(
+    (friend) =>
+      ({
+        __isNew__: false,
+        value: friend.id,
+        label: friend.name,
+        friend,
+      }) satisfies KnownSelectOption,
+  );
+
+  const defaultSelectValue = selectOptions.filter((option) =>
+    initialValue.existingFriendIds.includes(option.friend.id),
+  );
+  const [friends, setFriends] =
+    useState<readonly SelectOption[]>(defaultSelectValue);
+
+  function handleChange(newValue: readonly SelectOption[]) {
+    setFriends(newValue);
+    onChange(newValue);
+  }
+
+  return (
+    <CreatableSelect
+      closeMenuOnSelect={false}
+      blurInputOnSelect={false}
+      isMulti
+      defaultValue={defaultSelectValue}
+      filterOption={createFilter({
+        matchFrom: "start",
+      })}
+      value={friends}
+      options={selectOptions}
+      onChange={handleChange}
+      tabSelectsValue={false}
+      components={{ Input }}
+      styles={{
+        multiValue: (providedStyles, props) => ({
+          ...providedStyles,
+          backgroundColor: props.data.__isNew__
+            ? colors.reactSelectLightBlue
+            : providedStyles.backgroundColor,
+          fontStyle: props.data.__isNew__ ? "italic" : "normal",
+        }),
+      }}
+    />
+  );
+}
